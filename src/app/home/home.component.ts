6fee79329b8de718be8c4764ef5c537372839bc2
Michal <michalsasula@gmail.com>
2020-10-11 05:16:23 +0200

    finish home component html template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngleDown, faCog, faVideo, faGraduationCap, faTrophy, faFire, faBrain, faSyncAlt, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  angleDownIcon = faAngleDown;
+  caretRightIcon = faCaretRight;
+  cogIcon = faCog;
+  videoIcon = faVideo;
+  graduationIcon = faGraduationCap;
+  trophyIcon = faTrophy;
+  fireIcon = faFire;
+  brainIcon = faBrain;
+  reloadIcon = faSyncAlt;
+
+
 
   constructor() { }
 
